9062372858b4f3821ad2adbd1d03bc91fe98c94d
nurainnjwa <117926562+nurainnjwa@users.noreply.github.com>
2023-01-19 15:43:50 +0800

    Add files via upload

---
+++ wizardInfo-service/src/test/java/com/xeTraining/wizardInfoservice/WizardInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xeTraining.wizardInfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WizardInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
